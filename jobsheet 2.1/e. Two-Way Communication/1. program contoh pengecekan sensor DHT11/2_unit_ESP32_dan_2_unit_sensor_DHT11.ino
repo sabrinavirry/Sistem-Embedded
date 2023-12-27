d5b49535af61705b41dfeb77f6205048114d1e9b
sabrinavirry <151721571+sabrinavirry@users.noreply.github.com>
2023-12-27 17:35:33 +0700

    Update 2_unit_ESP32_dan_2_unit_sensor_DHT11.ino

--- jobsheet 2.1/e. Two-Way Communication/1. program contoh pengecekan sensor DHT11/2_unit_ESP32_dan_2_unit_sensor_DHT11.ino
+++ jobsheet 2.1/e. Two-Way Communication/1. program contoh pengecekan sensor DHT11/2_unit_ESP32_dan_2_unit_sensor_DHT11.ino
@@ -1,6 +1,7 @@
 // AISYAH NURUL AINI               (4.31.21.0.03)
 // BRIAN RAHMADITYA                (4.31.21.0.09)
 // SABRINA VIRRY TALITHA MEIRILLA  (4.31.21.0.23)
+
 #include "DHT.h"
 #define DHTPIN 4 // Digital pin connected to the DHT sensor
 // Uncomment whatever type you're using!
