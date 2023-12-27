60bf701d7a159ed6b40d9344845587287b188b59
sabrinavirry <151721571+sabrinavirry@users.noreply.github.com>
2023-12-27 17:27:26 +0700

    Update _receive_mac_Mengirim_Pesan_yang_Berbeda_Ke_Beberapa_Board_ESP3.ino

--- jobsheet 2.1/c. One-Way, One-to-Many Communication/2. Mengirim Pesan yang Berbeda Ke Beberapa Board ESP32/_receive_mac_Mengirim_Pesan_yang_Berbeda_Ke_Beberapa_Board_ESP3.ino
+++ jobsheet 2.1/c. One-Way, One-to-Many Communication/2. Mengirim Pesan yang Berbeda Ke Beberapa Board ESP32/_receive_mac_Mengirim_Pesan_yang_Berbeda_Ke_Beberapa_Board_ESP3.ino
@@ -1,11 +1,14 @@
 // AISYAH NURUL AINI               (4.31.21.0.03)
 // BRIAN RAHMADITYA                (4.31.21.0.09)
 // SABRINA VIRRY TALITHA MEIRILLA  (4.31.21.0.23)
+
 #include <esp_now.h>
 #include <WiFi.h>
 
-// REPLACE WITH YOUR RECEIVER'S MAC ADDRESS
-uint8_t receiverAddress[] = {0xC8, 0xF0, 0x9E, 0xF5, 0x2E, 0x6C};
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0xC8, 0xF0, 0x9E, 0xF4, 0x32, 0x7C};
+uint8_t broadcastAddress2[] = {0xC8, 0xF0, 0x9E, 0xF4, 0x4D, 0xD0};
+uint8_t broadcastAddress3[] = {0xC8, 0xF0, 0x9E, 0xF5, 0x3B, 0xAC};
 
 typedef struct test_struct1 {
   int x;
@@ -21,29 +24,23 @@ typedef struct test_struct3 {
   char message[20];
 } test_struct3;
 
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  // Check if the received data is from the expected sender
-  if (memcmp(mac_addr, receiverAddress, 6) == 0) {
-    if (data_len == sizeof(test_struct1)) {
-      test_struct1 *receivedData = (test_struct1 *)data;
-      Serial.print("Received data1: x=");
-      Serial.print(receivedData->x);
-      Serial.print(", y=");
-      Serial.println(receivedData->y);
-    } else if (data_len == sizeof(test_struct2)) {
-      test_struct2 *receivedData = (test_struct2 *)data;
-      Serial.print("Received data2: a=");
-      Serial.print(receivedData->a);
-      Serial.print(", b=");
-      Serial.println(receivedData->b);
-    } else if (data_len == sizeof(test_struct3)) {
-      test_struct3 *receivedData = (test_struct3 *)data;
-      Serial.print("Received data3: message=");
-      Serial.println(receivedData->message);
-    } else {
-      Serial.println("Received unknown data");
-    }
-  }
+test_struct1 data1;
+test_struct2 data2;
+test_struct3 data3;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4],
+           mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
@@ -54,22 +51,63 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+  esp_now_register_send_cb(OnDataSent);
 
-  esp_now_register_recv_cb(OnDataRecv);
-
-  // Add sender as a peer
-  esp_now_peer_info_t peerInfo;
+  // register first peer
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  memcpy(peerInfo.peer_addr, receiverAddress, 6);
-
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add sender as a peer");
+    Serial.println("Failed to add peer 1");
+    return;
+  }
+
+  // register second peer
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer 2");
+    return;
+  }
+
+  // register third peer
+  memcpy(peerInfo.peer_addr, broadcastAddress3, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer 3");
     return;
   }
 }
 
 void loop() {
-  // The data will be received and processed in the OnDataRecv callback
-  delay(1000);
+  // Generate random data for each struct
+  data1.x = random(0, 20);
+  data1.y = random(0, 20);
+
+  data2.a = random(0.0, 1.0);
+  data2.b = random(0.0, 1.0);
+
+  snprintf(data3.message, sizeof(data3.message), "Hello %d", random(1, 10));
+
+  // Send data to each receiver
+  esp_err_t result1 = esp_now_send(broadcastAddress1, (uint8_t *)&data1, sizeof(test_struct1));
+  if (result1 == ESP_OK) {
+    Serial.println("Sent data1 with success");
+  } else {
+    Serial.println("Error sending data1");
+  }
+
+  esp_err_t result2 = esp_now_send(broadcastAddress2, (uint8_t *)&data2, sizeof(test_struct2));
+  if (result2 == ESP_OK) {
+    Serial.println("Sent data2 with success");
+  } else {
+    Serial.println("Error sending data2");
+  }
+
+  esp_err_t result3 = esp_now_send(broadcastAddress3, (uint8_t *)&data3, sizeof(test_struct3));
+  if (result3 == ESP_OK) {
+    Serial.println("Sent data3 with success");
+  } else {
+    Serial.println("Error sending data3");
+  }
+
+  delay(2000);
 }
